22e9ee73642575ce371321b48ae0bda33f53f425
firehiwot kifle <firemammo87@gmail.com>
2022-06-23 18:47:13 -0500

    File added

---
+++ javaonline/src/com/fundamentals/Lesson3Ex.java
@@ -0,0 +1,18 @@
+package com.fundamentals;
+
+public class Lesson3Ex {
+
+     /*public static void main(String[] args){
+
+
+
+             //System.out.println("This is my first sentence\n I am new to java\n This java course is cool\n I am learning stuff every day ");
+         }*/
+     public void someMethod() {
+         System.out.println("This is my first sentence\n I am new to java\n This java course is cool\n I am learning stuff every day ");
+
+     }
+
+ }
+
+
---
+++ javaonline/src/com/fundamentals/Main.java
@@ -0,0 +1,26 @@
+package com.fundamentals;
+
+public class Main {
+    public static void main(String[] args){
+        //write your code here(single line commit)
+
+
+        /*
+        * multi line commit
+        * */
+
+        System.out.println("Hello Java");
+
+        House myHouse = new House();
+        Lesson3Ex myLesson3Ex = new Lesson3Ex();
+        myLesson3Ex.someMethod();
+
+
+}
+
+}
+
+
+
+
+
